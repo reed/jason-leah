d58f96d919238fd5c43bc30d9fa59e3c02420406
Nick Reed <reednj77@gmail.com>
2014-05-25 20:51:54 -0500

    Mail test successful

--- app/mailers/bridegroom.rb
+++ app/mailers/bridegroom.rb
@@ -1,7 +1,6 @@
 class Bridegroom < ActionMailer::Base
   default from: "Wedding RSVPs <no-reply@jason-leah.com>",
-          to: 'reednj77@gmail.com'
-          # to: Rails.env.development? ? 'reednj77@gmail.com' : ['reednj77@gmail.com', 'weberleah1@gmail.com', 'jrbartend22@gmail.com']
+          to: Rails.env.development? ? 'reednj77@gmail.com' : ['reednj77@gmail.com', 'weberleah1@gmail.com', 'jrbartend22@gmail.com']
 
   def rsvp(rsvp_data)
     @rsvp = rsvp_data
